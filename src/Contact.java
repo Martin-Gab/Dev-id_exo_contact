d6a6884b0738ee1e78af06dfbe5786cdd5e15228
Martin-Gab <martingabillet@gmail.com>
2023-03-13 21:14:54 +0100

    add new contact OK

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,36 @@
 public class Contact {
 
+    private String lastname, firstname, phoneNumber;
+
+    public Contact(String lastname, String firstname, String phoneNumber) {
+
+        this.lastname = lastname.toUpperCase();
+        this.firstname = firstname.substring(0,1).toUpperCase() + firstname.substring(1);
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/ContactApp.java
+++ src/ContactApp.java
@@ -8,6 +8,13 @@ public class ContactApp {
     String print;
     boolean isRunning;
     List<Contact> contactList;
+    Scanner sc;
+    int currentMenu;
+
+    final int MAINMENU = 0;
+    final int ADDMENU = 1;
+    final int SEARCHMENU = 2;
+    final int DISPLAYMENU = 3;
 
     public ContactApp() {
 
@@ -20,15 +27,16 @@ public class ContactApp {
                 + "***************************************\n";
         isRunning = true;
         contactList = new ArrayList<>();
+        sc = new Scanner(System.in);
+        currentMenu = MAINMENU;
 
         System.out.println(print);
 
-        displayMainMenu();
-        /*do {
-
+        do
             displayMainMenu();
+        while (isRunning);
 
-        } while (isRunning);*/
+        System.exit(0);
 
     }
 
@@ -46,27 +54,40 @@ public class ContactApp {
         retrieveUserInputForMenu();
     }
 
+    private void displayCreateMenu() {
+        currentMenu = ADDMENU;
+        print = "---------------------------------------\n"
+                + " 1 -> Entrez un nouveau contact\n"
+                + " 2 -> Retour au menu principal\n"
+                + ":";
+        System.out.println(print);
+        retrieveUserInputForMenu();
+    }
+
     private void retrieveUserInputForMenu() {
 
-        Scanner sc = new Scanner(System.in);
         try {
             int userChoice = sc.nextInt();
-            handleUserChoiceForMainMenu(userChoice);
+            sc.nextLine();
+            handleUserChoiceForMenus(userChoice);
         } catch (InputMismatchException ime) {
             print = "Veuillez renseigner un chiffre !";
             System.out.println(print);
             displayMainMenu();
         }
-        sc.close();
 
     }
 
-    private void handleUserChoiceForMainMenu(int userChoice) {
+    private void handleUserChoiceForMenus(int userChoice) {
 
         switch (userChoice){
 
             case 1:
-                System.out.println("Ajouter");
+                if (currentMenu == MAINMENU)
+                    displayAddContactSection();
+                else if (currentMenu == ADDMENU) {
+                    retrieveNewContactInfos();
+                }
                 break;
 
             case 2:
@@ -90,4 +111,40 @@ public class ContactApp {
         }
 
     }
+
+    private void displayAddContactSection() {
+
+        print = "---------------------------------------\n"
+                + " Ajouter un nouveau contact\n";
+        System.out.println(print);
+        retrieveNewContactInfos();
+
+    }
+
+    private void retrieveNewContactInfos() {
+
+        print = "Entrez son nom :";
+        System.out.println(print);
+        String lastname = sc.nextLine();
+
+        print = "Entrez son prénom :";
+        System.out.println(print);
+        String firstname = sc.nextLine();
+
+        print = "Entrez son numéro de téléphone :";
+        System.out.println(print);
+        String phoneNumber = sc.nextLine();
+
+        createNewContactAndAddToList(lastname, firstname, phoneNumber);
+
+    }
+
+    private void createNewContactAndAddToList(String lastname, String firstname, String phoneNumber) {
+
+        contactList.add(new Contact(lastname, firstname, phoneNumber));
+        displayCreateMenu();
+
+    }
+
+
 }
